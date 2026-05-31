7769d4b319e0c800f3c643e2e86a47001e1385a7
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-05-31 11:02:16 +0530

    Time: 3 ms (22.59%) | Memory: 27.1 MB (94.88%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int nodelete = arr[0]; //normal kadane ending
-        int onedelete = INT_MIN;
+        int nodelete = arr[0];
+        // int onedelete = arr[0]; // has no exactly one deleted element
+        int onedelete = INT_MIN; //will change later
         int res = arr[0];
-        int v1 = 0;
 
         for(int i =1; i<arr.size(); i++){
-            int prev_nodelete = nodelete;
-            int prev_onedelete = onedelete;
-            nodelete = max(nodelete + arr[i], arr[i]);
+            //save the previous answers of i index before moving to i+1;
+            int prevnodel = nodelete;
+            int preonedel = onedelete;
 
+            //no delete normal kadane best ending for max sum
+            nodelete = max(nodelete + arr[i],  arr[i]);
+            int v1;
             if(onedelete == INT_MIN){
                 v1 = arr[i];
             }
             else{
-                v1 = onedelete+arr[i];
+                v1 = onedelete + arr[i];
             }
 
-            onedelete = max(v1, prev_nodelete);
+            onedelete = max(v1, prevnodel);
 
-            res = max(res, max(nodelete, onedelete));
+            res = max(res, max(onedelete, nodelete));
         }
 
         return res;
