c5779b91b80af61bcf48fb4988eeb11e0ead98cc
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-06-05 08:58:56 +0530

    Time: 43 ms (57.06%) | Memory: 50.9 MB (19.10%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // no two pointer
-        // sliding window no as -ve no
-        //kadane no as no tracking of any ending look like to me or no min.max,product here asked exact equal to k
-        // then why prefix sum??
         int size = nums.size();
-        int total_sum = 0;
-        int running_sum = 0;
+        unordered_map<int, int> mp;
+        int sum = 0;
         int res = 0;
-        for(int x : nums){
-            total_sum += x;
-        }
-        unordered_map<int,int> mp;
-        mp[0] = 1;
-        int freq = 0;
-        for(int i = 0; i<size; i++){
-            running_sum += nums[i];
-            int ques = running_sum - k;
-            int freq = mp[ques];
-            res += freq;
-            mp[running_sum]++;
+
+        mp[0] = 1; // for sum = 0 it appear one time as for empty subarray
+                // in case if whole subarray upto sum is k, so sum - k = 0
+
+        for(int i =0; i<size; i++){
+            //running sum or sum at index i 
+            sum += nums[i];
+            int remain = sum - k;
+
+            //ask from hash map have it(how many times) seen remain in any of the previous sum frequeciues stored
+            res += mp[remain];
+            mp[sum]++; //stores the current sum till i will be used as sum - k for other indexes 
         }
         return res;
     }
