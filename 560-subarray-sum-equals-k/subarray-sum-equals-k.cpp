711f31df4be7f45125222ec123060fe821ebcb25
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-06-05 08:13:07 +0530

    Time: 55 ms (36.94%) | Memory: 51 MB (13.93%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // no two pointer
+        // sliding window no as -ve no
+        //kadane no as no tracking of any ending look like to me or no min.max,product here asked exact equal to k
+        // then why prefix sum??
         int size = nums.size();
-        int count = 0;
-        for(int i =0; i<size; i++){
-            int sum = 0;
-            for(int j =i; j<size; j++){
-                sum += nums[j];
-                if(sum == k)
-                count++;
-            }
+        int total_sum = 0;
+        int running_sum = 0;
+        int res = 0;
+        for(int x : nums){
+            total_sum += x;
         }
-        return count;
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int freq = 0;
+        for(int i = 0; i<size; i++){
+            running_sum += nums[i];
+            int ques = running_sum - k;
+            int freq = mp[ques];
+            res += freq;
+            mp[running_sum]++;
+        }
+        return res;
     }
 };
