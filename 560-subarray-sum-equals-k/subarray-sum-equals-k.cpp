02fdd76d31fac1793a3af003ee89563893e4845b
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-06-11 11:28:49 +0530

    Time: 65 ms (29.80%) | Memory: 51.2 MB (8.81%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int size = nums.size();
-        unordered_map<int, int> mp;
-        int sum = 0;
-        int res = 0;
+        unordered_map<int,int> mp;
 
-        mp[0] = 1; // for sum = 0 it appear one time as for empty subarray
-                // in case if whole subarray upto sum is k, so sum - k = 0
-
-        for(int i =0; i<size; i++){
-            //running sum or sum at index i 
-            sum += nums[i];
-            int remain = sum - k;
-
-            //ask from hash map have it(how many times) seen remain in any of the previous sum frequeciues stored
-            res += mp[remain];
-            mp[sum]++; //stores the current sum till i will be used as sum - k for other indexes 
+        mp[0] = 1;  
+        int current_sum = 0;
+        int remain = 0;
+        int count = 0;
+        for(int i = 0; i<nums.size(); i++){
+            current_sum +=nums[i];
+            remain = current_sum - k;
+            count+= mp[remain];
+            mp[current_sum]++;
         }
-        return res;
+        return count;
     }
 };
