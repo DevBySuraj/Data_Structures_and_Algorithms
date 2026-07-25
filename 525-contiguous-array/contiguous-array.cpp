0fa97c52717a56fb743bf07e46338340935709bd
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-07-25 21:59:47 +0530

    Time: 34 ms (13.90%) | Memory: 88.2 MB (73.19%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int size = nums.size();
-        for(int i =0; i<size; i++){
-            if(nums[i] == 0){
-                nums[i] = -1;
+        unordered_map<int,int> f;
+        int ones = 0, zeros = 0;
+        int res = 0, diff = 0;
+
+        for(int i=0; i<size; i++){
+            if(nums[i] == 0) zeros++;
+            else ones++;
+
+            diff = zeros - ones;
+            
+            // if diff =0 the while array from 0 index is correct
+            if(diff == 0){
+                res = max(res, i+1);
+                continue;
+            }
+
+
+            // if the diff is in the map then get f[diff] and calculate i-index the res only
+            // if find not used, then the diff value will be changed from first occurence to latest occurence
+            if(f.find(diff) != f.end()){
+                int index = f[diff];
+                res = max(res, i-index);
+            }
+            else{
+                // one time first diff occurence index insertion in the map
+                // will not be updated the next time
+                f[diff] = i;
             }
         }
-        unordered_map<int,int> mp;
-        mp[0] = -1;
-        int prefix = 0;
-        int ans = 0;
-        int first_occurence = 0;
-        int len = 0;
-        for(int i =0; i<size; i++){
-            prefix += nums[i];
-            if(mp.find(prefix) != mp.end()){
-            first_occurence = mp[prefix];
-            len = i - first_occurence;
-            ans = max(ans, len);
-            }
-            else mp[prefix] = i;
-        }
-        return ans;
+        return res;
     }
 };
