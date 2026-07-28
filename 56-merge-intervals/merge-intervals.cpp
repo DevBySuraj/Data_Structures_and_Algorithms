0b18a05532cc53596d26a8172c7e6d17360b7e79
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-07-28 12:37:19 +0530

    Time: 3 ms (88.89%) | Memory: 23.7 MB (82.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>>output;
+        int size = intervals.size();
         int current_start = intervals[0][0];
         int current_end = intervals[0][1];
         int next_start = 0;
         int next_end = 0;
-        int size = intervals.size();
+
 
         if(size == 1) return intervals;
+
+        vector<vector<int>> output;
 
         for(int i = 1; i<size; i++){
             next_start = intervals[i][0];
             next_end = intervals[i][1];
-            if(current_end >= next_start){ // they overlap
-                // current_end = next_end;
+            if(current_end >= next_start){ //overlaps here condition
                 current_end = max(current_end, next_end);
-                if(i == size -1)
-                output.push_back({current_start, current_end}); //pushes the current interval and
+                // if the whole array overlaps we will end here
+                if(i == size - 1){
+                    output.push_back({current_start, current_end});
+                }
             }
-            else{
-                output.push_back({current_start, current_end}); //pushes the current interval and
-                // moves to next interval
+
+            else{ // if the current intervals breaks we will push current merged intervals 
+            //and move to next intervals 
+                output.push_back({current_start, current_end});
+
                 current_start = intervals[i][0];
                 current_end = intervals[i][1];
 
                 if(i == size - 1)
-                output.push_back({current_start, current_end}); //pushes the current interval
+                output.push_back({current_start, current_end});
             }
-        }
+            }
         return output;
     }
 };
