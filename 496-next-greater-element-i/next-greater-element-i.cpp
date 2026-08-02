870d8aa5db011502841c743cefed930e8f3c5bca
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-08-02 12:35:28 +0530

    Time: 3 ms (40.70%) | Memory: 12.1 MB (97.24%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> s;
-        int size = nums2.size();
-        vector<int> out(nums1.size());
-        vector<int> output(nums2.size());
-        for(int i = size-1; i>=0; i--){
-            while(!s.empty() && nums2[i] > s.top()){
-                s.pop();
-            }
 
-            output[i] = s.empty() ? -1 : s.top();
-
-            s.push(nums2[i]);
-        }
-
-        unordered_map<int,int> mp;
-        for(int i = size-1; i>=0; i--){
-            mp[nums2[i]] = output[i];
-        }
-
+        vector<int> output(nums1.size(), -1);
         for(int i =0; i<nums1.size(); i++){
-            if(mp.find(nums1[i]) != mp.end()){
-                out[i] = mp[nums1[i]];
+            int element1 = nums1[i];
+
+            for(int j = 0; j<nums2.size(); j++){
+                if(element1 == nums2[j]){
+
+                    for(int k = j+1; k<nums2.size(); k++){
+                        if(element1 < nums2[k]){
+                        output[i] = nums2[k];
+                        break;
+                        }
+                    }
+                    break; // breaks j loop
+                }
             }
         }
-        return out;
+        return output;
     }
 };
