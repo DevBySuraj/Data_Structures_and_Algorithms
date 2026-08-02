d566cbb965ff300fc5e31247e7d48daaad7f64bb
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-08-02 15:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (15.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        vector<int> stack_output(nums2.size()); // output for the nums 2
+        vector<int>output(nums1.size());
+        for(int i = nums2.size() - 1;  i>=0; i--){
+            while(!s.empty()  && nums2[i] > s.top()){
+                s.pop();
+            }
 
-        vector<int> output(nums1.size(), -1);
-        for(int i =0; i<nums1.size(); i++){
-            int element1 = nums1[i];
+            stack_output[i] =  s.empty() ? -1 : s.top();
+            s.push(nums2[i]);
 
-            for(int j = 0; j<nums2.size(); j++){
-                if(element1 == nums2[j]){
+        }
 
-                    for(int k = j+1; k<nums2.size(); k++){
-                        if(element1 < nums2[k]){
-                        output[i] = nums2[k];
-                        break;
-                        }
-                    }
-                    break; // breaks j loop
-                }
+        unordered_map<int, int> mp;
+
+        for(int j = nums2.size() - 1; j>=0; j--){
+            mp[nums2[j]] = stack_output[j];
+        }
+
+        for(int k = 0; k<nums1.size(); k++){
+            if(mp.find(nums1[k]) != mp.end()){
+                output[k] = mp[nums1[k]];
             }
         }
+
         return output;
     }
 };
