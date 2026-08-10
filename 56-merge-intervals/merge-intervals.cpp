4cfc4e3b5a9b48bb53abea0d0c9d2cf7877cec90
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-08-10 22:42:48 +0530

    Time: 3 ms (88.78%) | Memory: 23.9 MB (31.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-
         int size = intervals.size();
-        int current_start = intervals[0][0];
-        int current_end = intervals[0][1];
-        int next_start = 0;
-        int next_end = 0;
-
-
-        if(size == 1) return intervals;
+        if(size == 1){
+            return intervals;
+        }
 
         vector<vector<int>> output;
 
+        int current_start = intervals[0][0];
+        int current_end = intervals[0][1];
+
         for(int i = 1; i<size; i++){
-            next_start = intervals[i][0];
-            next_end = intervals[i][1];
-            if(current_end >= next_start){ //overlaps here condition
-                current_start = current_start;
-                current_end = max(current_end, next_end);
-                continue;
+            int next_start = intervals[i][0];
+            int next_end = intervals[i][1];
+
+            if(current_end >= next_start){
+                current_end = max(current_end, next_end); // two cases
             }
 
+            else{
                 output.push_back({current_start, current_end});
 
-                current_start  = next_start;
-                current_end = next_end;
+                current_start = intervals[i][0];
+                current_end = intervals[i][1];
             }
-            output.push_back({current_start, current_end});
-        return output;
+        }
+        output.push_back({current_start, current_end});
+
+    return output;
     }
 };
