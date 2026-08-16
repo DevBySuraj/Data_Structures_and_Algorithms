a5f89ab34d3c7773cd65a7690e475c6201cb5676
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-08-17 00:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (24.28%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -3,30 +3,43 @@ public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
         int start1,start2,end1,end2;
         vector<vector<int>> output;
-
-        for(int i =0; i<firstList.size(); i++){
-            for(int j = 0; j<secondList.size(); j++){
-                if(firstList[i][0] < secondList[j][0]){
-                    start1 = firstList[i][0];
-                    end1 = firstList[i][1];
-                    start2 = secondList[j][0];
-                    end2 = secondList[j][1];
-                }
-                else{
-                    start1 = secondList[j][0];
-                    end1 = secondList[j][1];
-                    start2 = firstList[i][0];
-                    end2 = firstList[i][1];
-
-                }
-
-                if(end1 >= start2){
-                    int start = max(start1, start2);
-                    int end = min(end1, end2);
-                    output.push_back({start,end});
-                }
+        bool first = false;
+        int i = 0, j =0;
+        while(i< firstList.size() &&  j<secondList.size()){
+            if(firstList[i][0] < secondList[j][0]){
+                start1 = firstList[i][0];
+                end1 = firstList[i][1];
+                start2 = secondList[j][0];
+                end2 = secondList[j][1];
+                first = true;
             }
+            else{
+                start1 = secondList[j][0];
+                end1 = secondList[j][1];
+                start2 = firstList[i][0];
+                end2 = firstList[i][1];
+                first = false;
+            }
+
+            if(end1 >= start2){
+                output.push_back({max(start1, start2), min(end1, end2)});
+            }
+
+            if(first == true){
+                if(j == secondList.size() - 1) i++;
+                else if(end1 < secondList[j+1][0]) i++;
+                else j++; // >= case
+            }
+            else{// first == false
+                if(i == firstList.size() - 1) j++;
+                else if(end1 < firstList[i+1][0]) j++;
+                else i++; // >= case
+            }
+
+
         }
+
+  
         return output;
     }
 };
