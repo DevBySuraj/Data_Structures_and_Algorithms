e6e80c2c0e9a166f623a8351f574d753b67cedf1
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-08-17 00:43:54 +0530

    Time: 3 ms (43.00%) | Memory: 23.1 MB (24.28%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        int start1,start2,end1,end2;
+        int start, end;
         vector<vector<int>> output;
-        bool first = false;
         int i = 0, j =0;
         while(i< firstList.size() &&  j<secondList.size()){
-            if(firstList[i][0] < secondList[j][0]){
-                start1 = firstList[i][0];
-                end1 = firstList[i][1];
-                start2 = secondList[j][0];
-                end2 = secondList[j][1];
-                first = true;
-            }
-            else{
-                start1 = secondList[j][0];
-                end1 = secondList[j][1];
-                start2 = firstList[i][0];
-                end2 = firstList[i][1];
-                first = false;
-            }
+            start = max(firstList[i][0], secondList[j][0]);
+            end = min(firstList[i][1], secondList[j][1]);
 
-            if(end1 >= start2){
-                output.push_back({max(start1, start2), min(end1, end2)});
-            }
+            if(start <= end) output.push_back({start,end});
 
-            if(first == true){
-                if(j == secondList.size() - 1) i++;
-                else if(end1 < secondList[j+1][0]) i++;
-                else j++; // >= case
-            }
-            else{// first == false
-                if(i == firstList.size() - 1) j++;
-                else if(end1 < firstList[i+1][0]) j++;
-                else i++; // >= case
-            }
-
-
+            if(firstList[i][1] < secondList[j][1]) i++;
+            else j++;
         }
-
-  
         return output;
     }
 };
