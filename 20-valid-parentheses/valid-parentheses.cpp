96796cf50f173f758b757545c03bf11e55e18674
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-09-18 23:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+        if(s.length() == 1) return false;
+        if(s[0] == ')' || s[0] == ']' || s[0] =='}' ) return false;
 
         for(int i =0; i<s.length(); i++){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            if(s[i] == '(' || s[i] ==  '{' || s[i] ==  '[') {
                 st.push(s[i]);
+                continue;
             }
 
-            else{
-
-            if(st.empty()) return false; // if first char is wrong
-
-            else if( s[i] == ')' && st.top() == '('){
+            if(!st.empty() && s[i] == ')' && st.top() == '(')
                 st.pop();
-            }
-            else if(s[i] == ']' && st.top() == '['){
+            else if(!st.empty() && s[i] == ']' && st.top() == '[')
                 st.pop();
-            }
-            else if(s[i] == '}' && st.top() == '{' ){
-                st.pop();
-            }
-            else return false;
-
-            }
+            else if(!st.empty() && s[i] == '}' && st.top() == '{')
+                st.pop();  
+            else return false;  
         }
-
         return st.empty();
     }
 };
