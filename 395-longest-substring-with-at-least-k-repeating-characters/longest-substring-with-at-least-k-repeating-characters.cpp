0fde4a31eea558628ce638b7390222f361a21a91
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-09-21 09:26:46 +0530

    Time: 3 ms (63.46%) | Memory: 12.2 MB (36.43%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,56 +1,41 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int ans = 0;
 
-        // Try every possible number of unique characters
-        for (int targetUnique = 1; targetUnique <= 26; targetUnique++) {
+        if(s.length() < k)
+            return 0;
 
-            int freq[26] = {0};
+        unordered_map<char,int> freq;
 
-            int left = 0;
-            int uniqueChars = 0;
-            int charsAtLeastK = 0;
+        for(char c : s)
+            freq[c]++;
 
-            for (int right = 0; right < s.length(); right++) {
+        for(auto [ch, count] : freq) {
 
-                int idx = s[right] - 'a';
+            if(count < k) {
 
-                // New character enters the window
-                if (freq[idx] == 0)
-                    uniqueChars++;
+                int ans = 0;
+                string left = "";
+                string right = "";
 
-                freq[idx]++;
+                // split around invalid character
+                for(char c : s) {
 
-                // This character has just reached k
-                if (freq[idx] == k)
-                    charsAtLeastK++;
-
-                // Too many unique characters
-                while (uniqueChars > targetUnique) {
-
-                    int leftIdx = s[left] - 'a';
-
-                    // This character was satisfying >= k
-                    // and after removal it becomes < k
-                    if (freq[leftIdx] == k)
-                        charsAtLeastK--;
-
-                    freq[leftIdx]--;
-
-                    // Character completely removed
-                    if (freq[leftIdx] == 0)
-                        uniqueChars--;
-
-                    left++;
+                    if(c == ch) {
+                        ans = max(ans, longestSubstring(left, k));
+                        left = "";
+                    }
+                    else {
+                        left += c;
+                    }
                 }
 
-                // Every unique character has frequency >= k
-                if (uniqueChars == charsAtLeastK)
-                    ans = max(ans, right - left + 1);
+                ans = max(ans, longestSubstring(left, k));
+
+                return ans;
             }
         }
 
-        return ans;
+        return s.length();
     }
 };
